5d8e1291acfe1b8eb5e155c83bfd19e32cfaf0fd
Margolex <margaritalexina@gmail.com>
2023-02-06 18:16:10 +0300

    done

--- script.js
+++ script.js
@@ -2,24 +2,13 @@ const button = document.querySelector("button");
 const modalArea = document.querySelector(".modal");
 const box = document.querySelector('.modal_box')
 const body = document.querySelector("body");
-
 modalArea.classList.add('modal_change')
 
-const closeModal = event => {
-    const target = event.target;
-
-    if (target === modalArea && target != box) {
+const closeModal = () => {
         modalArea.style.visibility = "hidden";
         modalArea.style.opacity = 0;
-        body.classList.remove("modal_open"); 
-    }
-       
+        body.classList.remove("modal_open");   
 }
- const closeEsc = () => {
-    body.classList.remove("modal_open");
-     modalArea.style.visibility = "hidden";
-     modalArea.style.opacity = 0;
-};
     
 const openModal = () => {
     body.classList.add("modal_open");
@@ -28,8 +17,8 @@ const openModal = () => {
     }
 
 button.addEventListener("click", openModal);
-modalArea.addEventListener('click', closeModal);
-window.addEventListener("keyup", closeEsc);
+modalArea.addEventListener('click', (event) => { if (event.target === modalArea && event.target != box) closeModal()});
+window.addEventListener("keydown", (event) => { if (event.code === 'Escape') closeModal() });
 
 
 
